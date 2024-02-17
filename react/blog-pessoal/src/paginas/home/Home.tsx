a5ba8f2c48dea4c540577c762e80c44f144ef2e3
Thaiana Silva <thaianacelia@gmail.com>
2024-02-17 17:07:41 -0300

    atualizando pratica react 1

--- react/blog-pessoal/src/paginas/home/Home.tsx
+++ react/blog-pessoal/src/paginas/home/Home.tsx
@@ -4,7 +4,12 @@ import "./Home.css";
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="titulo">Home</h1>
+      <img
+        src="https://res.cloudinary.com/generation/image/upload/w_350,q_auto,f_auto/brazil/2022/04/Cover-1.png"
+        alt="Imagem Tela Inicial"
+        className="img"
+      />
     </>
   );
 }
